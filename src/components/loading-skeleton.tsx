8b62bcfaac55de2b3772e815c8a0d6ed8c84f428
prayasjain8765@gmail.com <prayasjain8765@gmail.com>
2025-06-13 11:10:00 +0530

    Comments Added

--- src/components/loading-skeleton.tsx
+++ src/components/loading-skeleton.tsx
@@ -2,6 +2,7 @@ import { Skeleton } from "./ui/skeleton";
 
 function WeatherSkeleton() {
   return (
+    // Used wen data is fetching from API to show loading 
     <div className="space-y-6">
       <div className="grid gap-6">
         <Skeleton className="h-[300px] w-full rounded-lg" />
